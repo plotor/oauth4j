63d58a06579b716dab776634796bf20dcd1ff3d7
zhenchao <zhenchao.wang@hotmail.com>
2017-02-18 14:43:30 +0800

    add user cancel redirect uri

--- sqls/t_oauth_app_info.sql
+++ sqls/t_oauth_app_info.sql
@@ -7,10 +7,13 @@ DROP TABLE IF EXISTS `t_oauth_app_info`;
 CREATE TABLE `t_oauth_app_info` (
   `app_id` bigint(20) unsigned NOT NULL,
   `app_name` varchar(255) NOT NULL,
+  `logo` varchar(1024) CHARACTER SET utf8mb4 NOT NULL DEFAULT '',
   `enable` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'app enable status, 0:disable, 1:enable',
   `redirect_uri` varchar(4096) DEFAULT NULL,
+  `cancel_redirect_uri` varchar(4096) DEFAULT NULL,
   `scope` varchar(1024) DEFAULT NULL,
   `token_validity` int(11) NOT NULL DEFAULT '7776000' COMMENT 'seconds',
+  `secret` varchar(255) NOT NULL,
   `level` int(2) NOT NULL DEFAULT '1',
   `creator_id` bigint(20) NOT NULL,
   `create_time` datetime NOT NULL,
