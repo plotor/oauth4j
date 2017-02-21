dd7c8477b87bb5f8ff61c9aaf69bccc3c471c250
zhenchao <zhenchao.wang@hotmail.com>
2017-02-21 23:32:03 +0800

    add UT

---
+++ src/test/java/org/zhenchao/passport/oauth/controllers/AuthorizationCodeGrantControllerTest.java
@@ -0,0 +1,23 @@
+package org.zhenchao.passport.oauth.controllers;
+
+import org.junit.Test;
+
+/**
+ * authorization code grant ut
+ *
+ * @author zhenchao.wang 2017-02-21 22:55
+ * @version 1.0.0
+ */
+public class AuthorizationCodeGrantControllerTest {
+
+    private static final long CLIENT_ID = 2882303761517520186L;
+
+    private static final String REDIRECT_URI = "http://www.zhenchao.com";
+
+    private static final String SCOPE = "1 4";
+
+    @Test
+    public void authorizeTest() throws Exception {
+    }
+
+}
