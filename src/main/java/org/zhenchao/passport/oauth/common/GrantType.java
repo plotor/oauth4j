5e0577e0439b2f820df586a0b4aa05dacdc6b686
wangzhenchao <wangzhenchao@xiaomi.com>
2017-04-20 18:06:06 +0800

    add refresh_token grant type

--- src/main/java/org/zhenchao/passport/oauth/common/GrantType.java
+++ src/main/java/org/zhenchao/passport/oauth/common/GrantType.java
@@ -14,7 +14,9 @@ public enum GrantType {
 
     PASSWORD_CREDENTIALS("password"),
 
-    CLIENT_CREDENTIALS("client_credentials");
+    CLIENT_CREDENTIALS("client_credentials"),
+
+    REFRESH_TOKEN("refresh_token");
 
     private String type;
 
